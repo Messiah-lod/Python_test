0de3a823f017787c6e6f0ee3eef96e62de9b4df3
Maks <diablo-lod@mail.ru>
2022-05-19 08:56:42 +0300

    test_commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Task 1")
+
+    print("Task 2")
+
+    print("Task 3")
+
+    print("Task 4")
+
+    print("Task 5")
+
+    print("Task 6")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
